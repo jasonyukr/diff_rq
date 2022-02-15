c495721dad91dffa3ad616e14aa7de50134ea240
Jason (Jin-Hwan) Yu <jin-hwan.yu@oracle.com>
2022-02-15 11:45:18 +0900

    Support -uNr param case

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,18 @@ use std::env;
 use std::process::Command;
 use std::path::Path;
 
+fn print_usage() {
+    println!("Usage :");
+    println!("  diff_rq dir1 dir2");
+    println!("  diff_rq --exclude .git dir1 dir2");
+    println!("  diff_rq -uNr --exclude .git dir1 dir2");
+}
+
 fn main() {
     let mut args: Vec<String> = env::args().collect();
     args.remove(0);
     if args.len() < 2 {
-        println!("Usage :");
-        println!("  diff_rq dir1 dir2");
-        println!("  diff_rq --exclude .git dir1 dir2");
+        print_usage();
         return;
     }
 
@@ -21,6 +26,22 @@ fn main() {
     }
     if !dir2.ends_with("/") {
         dir2.push('/');
+    }
+
+    // We can pass not-existing directory name especially when
+    // we run 'diff_rq -uNr' to get added/deleted file list fully
+    let mut total_add_mode = false;
+    if !Path::new(&dir1).is_dir() {
+        total_add_mode = true;
+    }
+    let mut total_delete_mode = false;
+    if !Path::new(&dir2).is_dir() {
+        total_delete_mode = true;
+    }
+    if total_add_mode == true && total_delete_mode == true {
+        // At least one directory should exist
+        print_usage();
+        return;
     }
 
     if let Ok(mut child) = Command::new("diff").arg("-rq").args(&args).stdout(Stdio::piped()).spawn() {
@@ -40,12 +61,18 @@ fn main() {
                             let file1_short = file1[dir1.len()..].to_string();
                             let file2_short = file2[dir2.len()..].to_string();
                             if file1_short == file2_short {
-                                println!("M │\x1b[1;34m\u{25fc}\u{25fc}\x1b[1;0m│ \x1b[1;34m{}\x1b[1;0m", file1_short); // modified: blue
+                                if total_add_mode { // "diff_rq -uNr" case
+                                    println!("A │\x1b[1;32m\u{00a0}\u{25fc}\x1b[1;0m│ \x1b[1;32m{}\x1b[1;0m", file1_short); // added: green
+                                } else if total_delete_mode { // "diff_rq -uNr" case
+                                    println!("D │\x1b[1;31m\u{25fc}\u{00a0}\x1b[1;0m│ \x1b[1;31m{}\x1b[1;0m", file1_short); // deleted: red
+                                } else { // normal case
+                                    println!("M │\x1b[1;34m\u{25fc}\u{25fc}\x1b[1;0m│ \x1b[1;34m{}\x1b[1;0m", file1_short); // modified: blue
+                                }
                                 continue;
                             }
-                        } else {
-                            panic!("Wrong directory prefix");
+                            panic!("Wrong filename");
                         }
+                        panic!("Wrong directory prefix");
                     } else if v[0] == "Only" && v[1] == "in" { // Only in PATH: FILE
                         let mut dir_name = v[2].clone();
                         if dir_name.ends_with(":") {
